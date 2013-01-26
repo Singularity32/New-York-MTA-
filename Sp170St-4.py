730972196f6483ce4b28b68fed4aad688b33decf
Venky <kvrn@Descent2.(none)>
2013-01-26 01:09:22 -0500

    170Th Stree

---
+++ Sp170St-4.py
@@ -0,0 +1,63 @@
+import csv
+import datetime
+from matplotlib.pylab import plt
+f1=open("./Turnstile/Turnstile-Data.txt","rb")
+f2=open("./Turnstile/Sp170St.txt","wb")
+rv=csv.reader(f1)
+ro=csv.writer(f2)
+print "The station we are analyzing is 170th Street on line 4"
+REMOTE="R243"; BOOTH="R284" # This is the code for the subway stop
+print "The weekday we consider is Wednesday Jan 9, 2013"
+CH_WKD="01-09-13"
+T_ent=-1     # Entry count on turnstile
+T_exit=-1   # Exit count on turnstile
+trun=[]
+SCP=""
+flag=-1
+for rw in rv:
+ if (rw[0]==BOOTH and rw[1]==REMOTE) :
+   flag=1
+   print len(rw)
+   if SCP!=rw[2]:
+     T_ent=-1     # Entry count on turnstile
+     T_exit=-1   # Exit count on turnstile
+     SCP=rw[2]
+   RC_LEN=5                # Length of each turnstile record at a given time (this excludes first three elements in each row)
+   j=3
+   while j<len(rw):
+    if rw[j]==CH_WKD:
+     if (T_ent <0):
+      T_ent=int(rw[j+3])      # Update entry count on turnstile
+      T_exit=int(rw[j+4])    # Update exit count on turnstile
+      T_tm=datetime.datetime.strptime(rw[j+1],'%H:%M:%S')     # Converting time to the datetime format in python
+      trun.append([SCP,T_tm, 0,0])      
+     else :
+      T_tm=datetime.datetime.strptime(rw[j+1],'%H:%M:%S')    # Converting time to the datetime format in python
+      trun.append([SCP,T_tm,int(rw[j+3])-T_ent ,int(rw[j+4])-T_exit])      
+      T_ent=int(rw[j+3]) 
+      T_exit=int(rw[j+4]) 
+    j=j+5   
+ elif flag >0 :
+  break;
+sum_EE =[]
+ct=0
+j=0
+T_ent_t=0; T_exit_t=0   # Total entry and exit for a given time from all the turnstiles respectively.
+SCP= trun[0][0]
+for line in trun :
+ print line[0],line[2],line[3]
+ if ct==0 and line[0]==SCP:
+  sum_EE.append([line[1], line[2],line[3]])
+ else:
+  ct=1 
+  if line[0]!=SCP :
+   j=0
+   SCP=line[0]
+  sum_EE[j][1]=sum_EE[j][1]+ line[2]
+  sum_EE[j][2]=sum_EE[j][2]+ line[3]
+  j=j+1
+
+for line in sum_EE:
+ print line, sum_EE.index(line)
+
+
